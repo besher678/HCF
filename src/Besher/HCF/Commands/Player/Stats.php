02403ecad82e33fd74a00b669c654afc542a6e4d
Besher678 <69113158+Besher678@users.noreply.github.com>
2021-06-20 20:10:11 +0200

    Added Alerts.php Added Repair.php Added Stats.php

---
+++ src/Besher/HCF/Commands/Player/Stats.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Besher\HCF\Commands\Player;
+
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\utils\CommandException;
+
+class Stats extends \pocketmine\command\Command
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        // TODO: Implement execute() method.
+    }
+}
